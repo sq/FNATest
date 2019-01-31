a1172537ff2469b66b095ec23716fb2dbbc920af
Katelyn Gadd <kg@luminance.org>
2019-01-31 14:26:33 -0800

    First commit

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FNATest {
+    public class TestGame : Game {
+        public GraphicsDeviceManager Graphics;
+
+        public TestGame () {
+            Graphics = new GraphicsDeviceManager(this);
+            Graphics.GraphicsProfile = GraphicsProfile.HiDef;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNATest {
+    class Program {
+        public static void Main (string[] args) {
+            using (var g = new TestGame())
+                g.Run();
+        }
+    }
+}
