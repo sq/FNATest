655f603d82d146469b37e27ef110c0c94bc37828
Katelyn Gadd <kg@luminance.org>
2019-05-26 23:09:58 -0700

    Checkpoint

--- Game.cs
+++ Game.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -16,7 +18,9 @@ namespace FNATest {
         public GraphicsDeviceManager Graphics;
         public DefaultMaterialSet Materials;
         public Texture2D Texture;
+#if !X64
         public FreeTypeFont Font;
+#endif
 
         public EmbeddedEffectProvider EffectProvider;
         public Material Vpos;
@@ -40,16 +44,26 @@ namespace FNATest {
         protected override void LoadContent () {
             base.LoadContent();
 
+#if !X64
             var provider = new EmbeddedFreeTypeFontProvider(RenderCoordinator);
             Font = provider.Load("FiraSans-Medium");
             Font.SizePoints = 20;
             Font.GlyphMargin = 4;
+#endif
 
             Texture = Texture2D.FromStream(Graphics.GraphicsDevice, Assembly.GetExecutingAssembly().GetManifestResourceStream("bunny"));
 
-            Materials = new DefaultMaterialSet(RenderCoordinator) {
-                ViewTransform = ViewTransform.CreateOrthographic(Graphics.PreferredBackBufferWidth, Graphics.PreferredBackBufferHeight)
-            };
+            try {
+                Materials = new DefaultMaterialSet(RenderCoordinator) {
+                    ViewTransform = ViewTransform.CreateOrthographic(Graphics.PreferredBackBufferWidth, Graphics.PreferredBackBufferHeight)
+                };
+            } catch (Exception exc) {
+                Console.WriteLine("Shader load failed.");
+                Console.WriteLine(exc);
+                Thread.Sleep(5000);
+                Environment.Exit(1);
+                return;
+            }
 
             EffectProvider = new EmbeddedEffectProvider(RenderCoordinator);
             Vpos = new Material(EffectProvider.Load("vpos"), "vpos");
@@ -68,17 +82,23 @@ namespace FNATest {
                 blendState: BlendState.Opaque,
                 samplerState: SamplerState.LinearClamp
             );
+
+#if !X64
             ir.DrawString(
                 Font, "Hello, World!", Vector2.Zero, 
                 blendState: BlendState.AlphaBlend,
                 material: Materials.ScreenSpaceShadowedBitmap
             );
+#endif
 
             var sg = ir.MakeSubgroup();
             sg.AutoIncrementLayer = true;
-            // sg.SetViewport(new Rectangle(64, 64, 1024, 512), true);
             sg.FillRectangle(new Rectangle(0, 0, 1024, 1024), Color.Black, customMaterial: Vpos);
             sg.SetViewport(null, true);
+        }
+
+        protected override void UnloadContent () {
+            Environment.Exit(1);
         }
     }
 }
